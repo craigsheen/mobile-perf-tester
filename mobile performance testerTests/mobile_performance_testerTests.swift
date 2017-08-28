f6d37eb671f9e2efdd627bddcfc9d9ed7eb26bff
Craig Sheen <craig_sheen@hotmail.com>
2017-08-28 11:43:59 +1000

    Initial Commit

---
+++ mobile performance testerTests/mobile_performance_testerTests.swift
@@ -0,0 +1,36 @@
+//
+//  mobile_performance_testerTests.swift
+//  mobile performance testerTests
+//
+//  Created by Craig Sheen on 28/8/17.
+//  Copyright © 2017 Craig Sheen. All rights reserved.
+//
+
+import XCTest
+@testable import mobile_performance_tester
+
+class mobile_performance_testerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
