340eb4ce9cd393753740bfee202c12514bd5d6fd
Craig Sheen <craig_sheen@hotmail.com>
2017-08-28 13:05:00 +1000

    First build

--- mobile performance tester/ViewController.swift
+++ mobile performance tester/ViewController.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        getURL();
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getURL() {
+        let alertController = UIAlertController(title: "URL?", message: "Please input the URL you would like to visit", preferredStyle: .alert)
+        
+        let confirmAction = UIAlertAction(title: "Go", style: .default) { (_) in
+            if let field = alertController.textFields?[0] {
+                let url = field.text
+                let request = URLRequest(url: URL(string: url!)!)
+                self.webView.loadRequest(request);
+            }
+        }
+        
+        alertController.addTextField { (textField) in
+            textField.placeholder = "URL"
+        }
+        
+        alertController.addAction(confirmAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 
 
